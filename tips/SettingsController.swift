aa0cb039a1e75ed83ffd7f890cabfe9050b9d0a4
sam <sam@wealthfront.com>
2015-08-23 17:37:57 -0700

    Add empty SettingsController

---
+++ tips/SettingsController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsController.swift
+//  tips
+//
+//  Created by Sam Sweeney on 8/23/15.
+//  Copyright © 2015 Wealthfront. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
